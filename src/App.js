5d057fde76a3067846bd64964b09eb9cfe03e429
tetianazaporozhets <web.smm.zty@gmail.com>
2023-09-10 18:49:00 +0200

    hw_20

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Button from "./components/Button";
+
+function App() {
+  return (
+    <div>
+      <div className="App">Hello!</div>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,6 @@
+import "./Button.css";
+
+const Button = () => {
+  return <button className="button">Click</button>;
+};
+export default Button;
