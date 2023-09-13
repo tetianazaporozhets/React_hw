f8e75a6b42573ff32397dc45276e13825a80dbd7
tetianazaporozhets <web.smm.zty@gmail.com>
2023-09-13 19:02:19 +0200

    hw_21

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import "./App.css";
 import Button from "./components/Button";
 
 function App() {
+  const click1 = () => console.log("Click 1");
+  const click2 = () => console.log("Click 2");
+  const click3 = () => console.log("Click 3");
   return (
     <div>
       <div className="App">Hello!</div>
-      <Button text="Click 1"></Button>
-      <Button text="Click 2"></Button>
-      <Button text="Click 3"></Button>
+      <Button text="Click 1" handleClick={click1}></Button>
+      <Button text="Click 2" handleClick={click2}></Button>
+      <Button text="Click 3" handleClick={click3}></Button>
     </div>
   );
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,8 @@
 import "./Button.css";
 
 const Button = (props) => {
-  const sayHello = () => console.log("Hello!");
   return (
-    <button className="button" onClick={sayHello}>
+    <button className="button" onClick={props.handleClick}>
       {props.text}
     </button>
   );
